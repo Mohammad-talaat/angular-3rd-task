e339e1b685a6d728cea406c3e65861b6ea7f70d7
Mohammad Talaat <eng.muhamedtalaat@gmail.com>
2022-08-23 18:52:13 +0200

    init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialUIModule } from './material-ui-module/material-ui.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialUIModule
   ],
   providers: [],
   bootstrap: [AppComponent]
