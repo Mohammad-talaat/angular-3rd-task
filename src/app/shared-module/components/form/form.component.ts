231797abf91671c062b6ef1a233df72c8987aa62
Mohammad Talaat <eng.muhamedtalaat@gmail.com>
2022-09-05 03:32:20 +0200

    add form controls

--- src/app/shared-module/components/form/form.component.ts
+++ src/app/shared-module/components/form/form.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators ,FormsModule,NgForm } from '@angular/forms';
 
- interface Options {
-  value: string;
-  viewValue: string;
- }
+  interface Options {
+    value: string;
+    viewValue: string;
+  }
+
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
@@ -12,7 +13,19 @@ import { FormBuilder, FormGroup, Validators ,FormsModule,NgForm } from '@angular
 })
 export class FormComponent implements OnInit {
 
-  constructor() { }
+  regiForm:any
+  constructor(fb:FormBuilder) {
+    this.regiForm = fb.group({
+      'name' : [null, Validators.required],
+      'email':[null, Validators.compose([Validators.required,Validators.email])],
+      'phone' : [null, Validators.required],
+      'address' : fb.group({
+        'street':[null, Validators.required],
+        'city':[null, Validators.required],
+      }),
+      'gender':[null, Validators.required],
+    });
+  }
 
   genderOptions: Options[] = [
     {value: 'male', viewValue: 'Male'},
@@ -21,4 +34,8 @@ export class FormComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onFormSubmit(form:NgForm)
+  {
+    console.log(form);
+  }
 }
